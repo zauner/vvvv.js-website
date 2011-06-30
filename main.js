df58f5bf30efe888d95c133931be6a92a4f79dfb
zauner <sagishi@zive.at>
2011-06-30 10:24:09 +0200

    added show patch link

--- main.js
+++ main.js
@@ -5,11 +5,26 @@ $(document).ready(function() {
   //initVVVV('vvvv_js/', 'full');
   
   $('#main').hide();
+  var vvvviewer;
   
   var graph = new VVVV.Core.Graph("main.v4p", function() {
     VVVV.Core.MainLoop.run(this);
-    //var vvvviewer = new VVVV.VVVViewer(this, '#patch');
     $('#main').show();
+  });
+  
+  $('a#showpatch').click(function() {
+    if (vvvviewer && vvvviewer!=undefined) {
+      vvvviewer.destroy();
+      delete vvvviewer;
+      vvvviewer = undefined;
+      $('#pagepatch').hide();
+      $(this).text("Show this site's patch");
+    }
+    else {
+      vvvviewer = new VVVV.VVVViewer(graph, '#pagepatch');
+      $('#pagepatch').show();
+      $(this).text("Hide this site's patch");
+    }
   });
   
   
