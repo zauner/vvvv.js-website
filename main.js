c73ee2917153a203186437140a9225a7f9ec1279
zauner <sagishi@zive.at>
2011-07-01 11:26:26 +0200

    use 'Patch' instead of obsolete 'Graph'

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 
-var graph2;
 
 $(document).ready(function() {
   //initVVVV('vvvv_js/', 'full');
@@ -8,7 +7,7 @@ $(document).ready(function() {
   var vvvviewer;
   var page_mainloop;
   
-  var graph = new VVVV.Core.Graph("main.v4p", function() {
+  var patch = new VVVV.Core.Patch("main.v4p", function() {
     page_mainloop = new VVVV.Core.MainLoop(this);
     $('#main').show();
   });
@@ -23,16 +22,10 @@ $(document).ready(function() {
       $(this).text("Show this site's patch");
     }
     else {
-      vvvviewer = new VVVV.VVVViewer(graph, '#pagepatch');
+      vvvviewer = new VVVV.VVVViewer(patch, '#pagepatch');
       $('#pagepatch').show();
       $(this).text("Hide this site's patch");
     }
-  });
-  
-  
-  
-  window.setTimeout(5, function() {
-    alert('stopped');
   });
   
   
